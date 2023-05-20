106f7a1327d1d17dcde7fe9e993433f64e4300da
AibarIbray <aibar.s.ibrayev@gmail.com>
2023-05-20 22:17:53 +0600

    kafka to docket

--- src/main/java/com/example/kafkaconsumerservice/kafka/KafkaConfiguration.java
+++ src/main/java/com/example/kafkaconsumerservice/kafka/KafkaConfiguration.java
@@ -1,4 +1,5 @@
 package com.example.kafkaconsumerservice.kafka;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +17,14 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "parking-sensor-group");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
+
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
--- src/main/java/com/example/kafkaconsumerservice/kafka/KafkaConsumer.java
+++ src/main/java/com/example/kafkaconsumerservice/kafka/KafkaConsumer.java
@@ -1,6 +1,5 @@
 package com.example.kafkaconsumerservice.kafka;
 
-
 import com.example.kafkaconsumerservice.model.ParkingSensor;
 import com.example.kafkaconsumerservice.model.ParkingSpot;
 import com.example.kafkaconsumerservice.service.ParkingService;
@@ -10,36 +9,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
 public class KafkaConsumer {
     @Autowired
     private ParkingService parkingService;
     @Autowired
     private ParkingSpotWebSocketHandler parkingSpotWebSocketHandler;
+
     @KafkaListener(topics = "parking-sensor-topic", groupId = "parking-sensor-group")
     public void consume(String message) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         ParkingSensor parkingSensor = objectMapper.readValue(message, ParkingSensor.class);
 
-
         System.out.println("Received parking sensor data: " + parkingSensor.toString());
 
-        //parkingSpotWebSocketHandler.sendParkingSpotUpdate(message);
         ParkingSpot parkingSpot = parkingService.getParkingSpotBySensorId(parkingSensor.getSensorId());
-        // Обновляем состояние парковочного места
+
         System.out.println(parkingSpot.toString());
-        if(parkingSpot.getIsOccupied() != parkingSensor.getIsOccupied() && parkingSensor.getIsOccupied() == true){
-            // Сохраняем обновленное состояние парковочного места
+        if (parkingSpot.getIsOccupied() != parkingSensor.getIsOccupied() && parkingSensor.getIsOccupied()) {
             parkingService.startTimer(parkingSpot.getId(), parkingSensor.getIsOccupied());
-        } else if (parkingSpot.getIsOccupied() != parkingSensor.getIsOccupied() && parkingSensor.getIsOccupied() == false) {
+        } else if (parkingSpot.getIsOccupied() != parkingSensor.getIsOccupied() && !parkingSensor.getIsOccupied()) {
             parkingService.stopParkingSession(parkingSpot.getId());
         }
-        //System.out.println("Data from postgre: "+ parkingSpot);
 
-        // Отправляем обновленное состояние парковочного места через веб-сокеты
         parkingSpotWebSocketHandler.sendParkingSpotUpdate(objectMapper.writeValueAsString(parkingSpot));
     }
 }
-
