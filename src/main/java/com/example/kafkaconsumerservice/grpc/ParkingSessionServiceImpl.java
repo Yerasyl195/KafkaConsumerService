44190e50b00e54012247e2fa4817db5ce213093d
Yerasyl Kenesbek <ekenesbek@Yerasyls-MacBook-Pro.local>
2023-06-05 00:29:57 +0300

    Parking session added

--- src/main/java/com/example/kafkaconsumerservice/grpc/ParkingSessionServiceImpl.java
+++ src/main/java/com/example/kafkaconsumerservice/grpc/ParkingSessionServiceImpl.java
@@ -1,40 +1,29 @@
 package com.example.kafkaconsumerservice.grpc;
 
-
-import com.example.parking.ParkingSession;
-import com.example.parking.ParkingSessionServiceGrpc;
-import com.example.parking.StreamRequest;
 import io.grpc.stub.StreamObserver;
+import kz.aparking.parkingsession.ParkingSessionOuterClass;
+import kz.aparking.parkingsession.ParkingSessionServiceGrpc;
 import org.springframework.stereotype.Service;
 
-import java.util.Iterator;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ParkingSessionServiceImpl extends ParkingSessionServiceGrpc.ParkingSessionServiceImplBase {
-    private final ConcurrentLinkedQueue<StreamObserver<ParkingSession>> observers = new ConcurrentLinkedQueue<>();
-
-    public void addParkingSession(ParkingSession parkingSession) {
-        Iterator<StreamObserver<ParkingSession>> iterator = observers.iterator();
-        while (iterator.hasNext()) {
-            StreamObserver<ParkingSession> observer = iterator.next();
+    private final ConcurrentHashMap<Long, StreamObserver<ParkingSessionOuterClass.ParkingSession>> observers = new ConcurrentHashMap<>();
+    public void addParkingSession(Long userId, ParkingSessionOuterClass.ParkingSession parkingSession) {
+        StreamObserver<ParkingSessionOuterClass.ParkingSession> observer = observers.get(userId);
+        if(observer != null) {
             try {
                 observer.onNext(parkingSession);
             } catch (Exception e) {
-                // Если произошла ошибка при отправке данных, удаляем observer из списка
-                // Необходимо отметить, что метод remove() итератора CopyOnWriteArrayList не поддерживается, поэтому вместо него используется observers.remove().
-                observers.remove(observer);
+                observers.remove(userId);
             }
         }
     }
-
     @Override
-    public void streamParkingSessions(StreamRequest request, StreamObserver<ParkingSession> responseObserver) {
-        // Добавить observer в список при подключении нового клиента
-        observers.add(responseObserver);
-
-        // Если серверу нужно отключить соединение с клиентом, он может вызвать responseObserver.onCompleted();
-        // Если серверу нужно сообщить клиенту об ошибке, он может вызвать responseObserver.onError(t);
+    public void streamParkingSessions(ParkingSessionOuterClass.StreamRequest request, StreamObserver<ParkingSessionOuterClass.ParkingSession> responseObserver) {
+        Long userId = request.getUserId();
+        observers.put(userId, responseObserver);
     }
 }
 
